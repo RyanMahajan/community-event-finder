09ae9d251c003a4e5857d1cb33e7131c83b12994
MidgetHiC <whavly@gmail.com>
2025-11-09 18:39:55 -0500

    Starting TikTok (hotspot) clone

---
+++ hotspot/app/(tabs)/_layout.tsx
@@ -0,0 +1,21 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Text } from 'react-native'
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#000',
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: () => <Text>Home</Text>,
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ hotspot/app/_layout.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useFonts } from 'expo-font'
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import 'react-native-reanimated';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  React.useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded])
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      </Stack>
+  );
+}
