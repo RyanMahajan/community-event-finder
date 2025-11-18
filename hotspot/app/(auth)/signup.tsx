b6b6505057feb181872a68c817e0933f8ae4aab8
MidgetHiC <whavly@gmail.com>
2025-11-17 22:43:44 -0500

    Added blank template pages, new auth Screen.Stack for Login and Signup navigation, and used expo router to link them to the main _layout (file based routing)

--- hotspot/app/(auth)/signup.tsx
+++ hotspot/app/(auth)/signup.tsx
@@ -1,10 +1,10 @@
 import { Text, View } from 'react-native';
 import '../../global.css';
 
-export default function HomeScreen() {
+export default function () {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-black font-bold text-3xl">Home</Text>
+      <Text className="text-black font-bold text-3xl">Signup</Text>
     </View>
   );
 }
