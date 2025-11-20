e821dba60cddf74ccd9ac0a423eecf78bf42cde9
MidgetHiC <whavly@gmail.com>
2025-11-20 00:00:11 -0500

    working on login and sign up auth; not finished. nite nite

--- hotspot/app/(auth)/index.tsx
+++ hotspot/app/(auth)/index.tsx
@@ -1,17 +1,46 @@
 import { useRouter } from 'expo-router';
-import { Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import '../../global.css';
 
 export default function () {
   const router = useRouter();
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleLogin = () => {
+    console.log(email, password)
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <TouchableOpacity
-        className="bg-black px-4 py-2 rounded-lg"
-        onPress={() => router.push('/(tabs)')}
-      >
-        <Text className="text-white font-bold text-lg">Login</Text>
-      </TouchableOpacity>
+      <View className="w-full p-4">
+        <Text className="text-black font-bold text-3xl text-center mb-4">Login</Text>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          className="bg-white p-4 rounded-lg border border-gray-300 w-full mb-4"
+        />
+        <TextInput
+          secureTextEntry={true}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          className="bg-white p-4 rounded-lg border border-gray-300 w-full mb-4"
+        />
+        <TouchableOpacity
+          className="bg-black px-4 py-2 rounded-lg"
+          onPress={handleLogin}
+        >
+          <Text className="text-white font-bold text-lg text-center">Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          onPress={() => router.push('/signup')}
+        >
+         <Text className="text-black font-semibold text-lg text-center mt-3">Signup</Text> 
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- hotspot/app/(auth)/signup.tsx
+++ hotspot/app/(auth)/signup.tsx
@@ -1,10 +1,54 @@
-import { Text, View } from 'react-native';
+import { supabase } from '@/utils/supabase';
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import '../../global.css';
 
 export default function () {
+  const router = useRouter();
+  const [username, setUsername] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSignup = async () => {
+    console.log(username, email, password)
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    })
+    console.log(data, error);
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-black font-bold text-3xl">Signup</Text>
+      <View className="w-full p-4">
+        <Text className="text-black font-bold text-3xl text-center mb-4">Signup</Text>
+        <TextInput
+          placeholder="Username"
+          value={username}
+          onChangeText={setUsername}
+          className="bg-white p-4 rounded-lg border border-gray-300 w-full mb-4"
+        />
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          className="bg-white p-4 rounded-lg border border-gray-300 w-full mb-4"
+        />
+        <TextInput
+          secureTextEntry={true}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          className="bg-white p-4 rounded-lg border border-gray-300 w-full mb-4"
+        />
+        <TouchableOpacity
+          className="bg-black px-4 py-2 rounded-lg"
+          onPress={handleSignup}
+        >
+          <Text className="text-white font-bold text-lg text-center">Signup</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
